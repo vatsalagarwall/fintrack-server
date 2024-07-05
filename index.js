fdbb780ea41e9eafb3073b5774578a6469a75d1f
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-07-05 13:15:22 +0530

    commit

--- index.js
+++ index.js
@@ -19,6 +19,14 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(cors())
 
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000/"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
 //routes
 //user routes
 app.use('/api/v1/users', require('./routes/userRoute'))
