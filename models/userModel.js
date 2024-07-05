23740f223c7db2cee2270a9592c4307a95300296
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-07-05 19:10:48 +0530

    commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 //schema design
 const userSchema = new mongoose.Schema(
@@ -19,6 +20,17 @@ const userSchema = new mongoose.Schema(
     },
     { timestamps: true }
 );
+
+userSchema.pre('save', async function (next) {
+    // Check if the password field has been modified
+    if (this.isModified('password')) {
+        // Generate a salt with a cost factor of 10
+        const salt = await bcrypt.genSalt(10);
+        // Hash the password using the generated salt
+        this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
 
 //export
 const userModel = mongoose.model("users", userSchema);
