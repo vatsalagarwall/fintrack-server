a0fc19dcf63ea60df2c57e6fba62e1d096687314
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-07-05 19:21:46 +0530

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,53 +3,23 @@ const bcrypt = require('bcrypt');
 
 
 //login callback
-// const loginController = async (req, res) => {
-//     try {
-//         const { email, password } = req.body
-//         const user = await userModel.findOne({ email, password })
-//         if (!user) {
-//             return user.status(404).send('User Not Found')
-//         }
-//         res.status(200).json({
-//             success: true,
-//             user,
-//         });
-//     }
-//     catch (error) {
-//         res.status(400).json({ success: false, error })
-//     }
-// };
 const loginController = async (req, res) => {
     try {
-        const { email, password } = req.body;
-
-        // Find the user by email
-        const user = await userModel.findOne({ email });
-
+        const { email, password } = req.body
+        const user = await userModel.findOne({ email, password })
         if (!user) {
-            return res.status(404).send('User Not Found');
+            return user.status(404).send('User Not Found')
         }
-
-        // Compare the provided password with the stored hashed password
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (!isMatch) {
-            return res.status(400).send('Invalid email or password');
-        }
-
-        // If passwords match, return success message or token
         res.status(200).json({
             success: true,
             user,
         });
-
-        // Optionally, you could create a JWT token here for authenticated sessions
-        // const token = createToken(user); // Implement createToken function
-        // res.status(200).json({ token });
-    } catch (error) {
-        res.status(400).json({ success: false, error });
+    }
+    catch (error) {
+        res.status(400).json({ success: false, error })
     }
 };
+
 
 //register callback
 const registerController = async (req, res) => {
--- models/userModel.js
+++ models/userModel.js
@@ -21,16 +21,6 @@ const userSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-userSchema.pre('save', async function (next) {
-    // Check if the password field has been modified
-    if (this.isModified('password')) {
-        // Generate a salt with a cost factor of 10
-        const salt = await bcrypt.genSalt(10);
-        // Hash the password using the generated salt
-        this.password = await bcrypt.hash(this.password, salt);
-    }
-    next();
-});
 
 //export
 const userModel = mongoose.model("users", userSchema);
